915b3caf24d9858bc160026d88df4f58a0c603c1
Oleksandra Shapovalenko <shapovalenko.aleksandra@gmail.com>
2024-08-12 09:56:58 +0300

    JavaScriptConditionsAndLoops

---
+++ jsBasics1/task1.js
@@ -0,0 +1,13 @@
+const averageGrade = 76;
+if (averageGrade > 90 && averageGrade <= 100) {
+    console.log('Відмінно!');
+} else if (averageGrade > 80) {
+    console.log('Дуже добре!');
+} else if (averageGrade > 70) {
+    console.log('Добре!');
+} else if (averageGrade >= 60) {
+    console.log('Задовільно!');
+} else if (averageGrade < 60) {
+    console.log('Незадовільно!');
+}
+
